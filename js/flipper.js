37ccc5e4a174896a1680622bc2d4e98d812701f2
Tristan <thrawst88@gmail.com>
2015-02-23 23:17:41 -0700

    basic flipping, using es6 class

--- js/flipper.js
+++ js/flipper.js
@@ -5,17 +5,14 @@ var miniBitWidth =0;
 var miniBitHeight=0;
 var colors = ['#000', '#00f', '#0f0', '#0ff', '#f00', '#f0f'];
 var variety = 2;
-var grid      = [];
-var resetGrid = [];
-var moves     = [];
 var moveNum = 0;
-var mode    = 0;
-var boardRows;
-var boardCols;
+var board;
 
 $(document).ready(function() {
 	init();
 });
+
+function test(){console.log("what now?");}
 
 function init() {
 	bitWidth  = $('#bit').width();
@@ -27,15 +24,16 @@ function init() {
 
 	var loc = document.location.href;
 	if(loc.substr(-5) === '.html' || loc.substr(-1) === '/') {
-		drawBoard(3,3);
+		//drawBoard(3,3);
+    board = new Board(3,3);
 	}
 	else {
 		var args = loc.split('?')[1].split('+');
 
-		mode = parseInt(args[0]);
-		variety = parseInt(args[1]);
-		boardRows = parseInt(args[2]);
-		boardCols = parseInt(args[3]);
+		let mode = parseInt(args[0]);
+		let variety = parseInt(args[1]);
+		let boardRows = parseInt(args[2]);
+		let boardCols = parseInt(args[3]);
 		drawBoard(boardRows, boardCols);
 
 		resetGrid = new Array();
@@ -53,312 +51,343 @@ function init() {
 	}
 }
 
-function drawBoard(rows, cols) {
-	var k=0;
+class Board{
+  constructor(rows=3, cols=3, variety=2, colors=['#000', '#00f', '#0f0', '#0ff', '#f00', '#f0f']){
+    this.rows = rows;
+    this.cols = cols;
+    this.variety = variety;
+    this.colors = colors;
+    
+    this.grid      = [];
+    this.resetGrid = [];
+    this.moves     = [];
 
-	$('#status').empty();
-	$('#board').empty();
+    this.draw();
+  }
 
-	moveNum = 0;
-	boardRows = rows;
-	boardCols = cols;
+  draw() {
+    var k=0;
+    
+    $('#status').empty();
+    $('#board').empty();
 
-	for(var i=0; i<rows; i++) {
-		var width = `${bitWidth * cols + bitMargin * cols}px`;
-		var inner = '';
-		inner += `<div id="bitRow" style="width:${width};">`;
-		for(var j=0; j<cols; j++, k++){
-			inner += `<div id="bit" class="${k}"` +
-			`style="background-color:${colors[0]};"` + 
-			`onclick="flipBits(${k})"></div>`;
-		}
-		$('#board').append(inner);
-	}
+    for(var i=0; i<this.rows; i++) {
+      var cells = [];
+      var width = `${bitWidth * this.cols + bitMargin * this.cols}px`;
+      var inner = '';
+      inner += `<div id="bitRow" style="width:${width};">`;
+      for(var j = 0; j < this.cols; j++, k++){
+        inner += `<div id="bit" class="${k}"` +
+        `style="background-color:${this.colors[0]};"></div>`;
+        cells.push(k);
+      }
+      $('#board').append(inner);
+      cells.forEach(el => $(`.${el}`).bind('click', () => this.flip(el)) );
+    }
 
-	width = (miniBitWidth * variety + bitMargin * variety)+'px';
-	var pat = `<div id="bitRow" style="width:${width};">`;
-	for(var i=0; i<variety; i++) {
-		pat += `<div id="miniBit" style="background-color:${colors[i]};"></div>`;
-	}
-	pat += '</div>';
-	$('#pattern').html(pat);
+    width = `${miniBitWidth * this.variety + bitMargin * this.variety}px`;
+    var pat = `<div id="bitRow" style="width:${width};">`;
+    for(var i = 0; i < this.variety; i++) {
+      pat += `<div id="miniBit" style="background-color:${this.colors[i]};"></div>`;
+    }
+    pat += '</div>';
+    $('#pattern').html(pat);
 
-	moves     = [];
-	resetGrid = [];
-	grid      = [];
+    while (this.grid.length < this.cols*this.rows) {
+      this.grid.push(0);
+    }
 
-	while (grid.length < cols*rows) {
-		grid.push(0);
-	}
+    this.randomize();
 
-	randomize();
+    this.grid.forEach((el, index) => this.resetGrid[index] = el); 
+  }
 
-	grid.forEach((el, index) => resetGrid[index] = el);
+  randomize() {
+    var boardSize = this.rows * this.cols;
+    for(var i = 0; i < boardSize * 5; i++) {
+      this.reverseFlip(this.rand(boardSize));
+    }
+  }
+
+  rand(max) {
+    return Math.floor(Math.random() * (max));
+  }
+
+  resetBoard() {
+    this.resetGrid.forEach((el, index) => this.grid[index] = el);
+    this.resetGrid.forEach((el, index) => $('.'+index).css('background-color', this.colors[el]) );
+  }
+
+  flip(theBit) {
+    this.grid[theBit] = (this.grid[theBit] + 1) % this.variety;
+    $(`.${theBit}`).css('background-color', this.colors[this.grid[theBit] % this.variety]);
+  }
+
+  reverseFlip(theBit) {
+    this.grid[theBit] = (this.grid[theBit] - 1);
+
+    if (this.grid[theBit] < 0) {
+      this.grid[theBit] = this.variety - 1;
+    }
+
+    $(`.${theBit}`).css('background-color', this.colors[this.grid[theBit] % this.variety]);
+  }
 }
 
-function reset() {
-	resetGrid.forEach((el, index) => grid[index] = el);
-	resetGrid.forEach((el, index) => $('.'+index).css('background-color', colors[el]) );
-}
+// function replay() {
+// 	reset();
+// 	var time = 0;
+// 	var moveCount = moves.length;
+// 	//play back in 10 seconds, or 1 move/750ms.
+// 	var inc = Math.min(10000 / moveCount, 500);
 
-function replay() {
-	reset();
-	var time = 0;
-	var moveCount = moves.length;
-	//play back in 10 seconds, or 1 move/750ms.
-	var inc = Math.min(10000 / moveCount, 500);
+// 	for (var i=0; i < moveCount; i++) {
+// 		time += inc;
+// 		if (mode === 0) {
+// 			setTimeout(flipAdjacent, time, moves[i]);
+// 		}
+// 		else if (mode === 1) {
+// 			setTimeout(flipStar, time, moves[i]);
+// 		}
+// 		else if (mode === 2) {
+// 			setTimeout(flipDouble, time, moves[i]);
+// 		}
+// 	}
+// }
 
-	for (var i=0; i < moveCount; i++) {
-		time += inc;
-		if (mode === 0) {
-			setTimeout(flipAdjacent, time, moves[i]);
-		}
-		else if (mode === 1) {
-			setTimeout(flipStar, time, moves[i]);
-		}
-		else if (mode === 2) {
-			setTimeout(flipDouble, time, moves[i]);
-		}
-	}
-}
+// function flipBits(abit) {
+// 	if (mode === 0) {
+// 		flipAdjacent(abit);
+// 	}
+// 	else if (mode === 1) {
+// 		flipStar(abit);
+// 	}
+// 	else if (mode === 2) {
+// 		flipDouble(abit);
+// 	}
 
-function flipBits(abit) {
-	if (mode === 0) {
-		flipAdjacent(abit);
-	}
-	else if (mode === 1) {
-		flipStar(abit);
-	}
-	else if (mode === 2) {
-		flipDouble(abit);
-	}
+// 	moveNum++;
+// 	moves.push(abit);
 
-	moveNum++;
-	moves.push(abit);
+// 	if (solved()) {
+// 		$('#status').html(`Solved in ${moveNum} moves! <br/>` +
+// 		'<button onclick="replay()">Replay</button>' +
+// 		'<button onclick="genPermalink();">Permalink</button>' +
+// 		'<input type="text" id="pl" size="25"> </input>');
 
-	if (solved()) {
-		$('#status').html(`Solved in ${moveNum} moves! <br/>` +
-		'<button onclick="replay()">Replay</button>' +
-		'<button onclick="genPermalink();">Permalink</button>' +
-		'<input type="text" id="pl" size="25"> </input>');
+// 		moveNum = 0;
+// 	}
+// }
 
-		moveNum = 0;
-	}
-}
+// function flipAdjacent(theBit) {
+// 	//flip adjacent tiles
+// 	flip(theBit);
+// 	//top
+// 	if (grid[theBit-boardCols]+1) {
+// 		flip(theBit-boardCols);
+// 	}
 
-function flipAdjacent(theBit) {
-	//flip adjacent tiles
-	flip(theBit);
-	//top
-	if (grid[theBit-boardCols]+1) {
-		flip(theBit-boardCols);
-	}
+// 	//bottom
+// 	if (grid[theBit+boardCols]+1) {
+// 		flip(theBit+boardCols);
+// 	}
 
-	//bottom
-	if (grid[theBit+boardCols]+1) {
-		flip(theBit+boardCols);
-	}
+// 	//left
+// 	if (grid[theBit-1]+1 && getRow(theBit) === getRow(theBit-1)) {
+// 		flip(theBit-1);
+// 	}
 
-	//left
-	if (grid[theBit-1]+1 && getRow(theBit) === getRow(theBit-1)) {
-		flip(theBit-1);
-	}
+// 	//right
+// 	if (grid[theBit+1]+1 && getRow(theBit) === getRow(theBit+1)) {
+// 		flip(theBit+1);
+// 	}
+// }
 
-	//right
-	if (grid[theBit+1]+1 && getRow(theBit) === getRow(theBit+1)) {
-		flip(theBit+1);
-	}
-}
+// function flipStar(theBit) {
+// 	flip(theBit);
+// 	//above
+// 	var i = 1;
+// 	while (grid[theBit-boardCols*i]+1) {
+// 		flip(theBit-boardCols*i);
+// 		i++;
+// 	}
 
-function flipStar(theBit) {
-	flip(theBit);
-	//above
-	var i = 1;
-	while (grid[theBit-boardCols*i]+1) {
-		flip(theBit-boardCols*i);
-		i++;
-	}
+// 	//below
+// 	i = 1;
+// 	while (grid[theBit+boardCols*i]+1) {
+// 		flip(theBit+boardCols*i);
+// 		i++;
+// 	}
 
-	//below
-	i = 1;
-	while (grid[theBit+boardCols*i]+1) {
-		flip(theBit+boardCols*i);
-		i++;
-	}
+// 	//left
+// 	i = 1;
+// 	while (grid[theBit-i]+1 && getRow(theBit) === getRow(theBit-i)) {
+// 		flip(theBit-i);
+// 		i++;
+// 	}
 
-	//left
-	i = 1;
-	while (grid[theBit-i]+1 && getRow(theBit) === getRow(theBit-i)) {
-		flip(theBit-i);
-		i++;
-	}
+// 	//right
+// 	i = 1;
+// 	while (grid[theBit+i]+1 && getRow(theBit) === getRow(theBit+i)) {
+// 		flip(theBit+i);
+// 		i++;
+// 	}
+// }
 
-	//right
-	i = 1;
-	while (grid[theBit+i]+1 && getRow(theBit) === getRow(theBit+i)) {
-		flip(theBit+i);
-		i++;
-	}
-}
+// function flipDouble(theBit) {
+// 	//flip adjacent tiles
+// 	flip(theBit)
+// 	//top
+// 	if (grid[theBit-boardCols]+1) {
+// 		flip(theBit-boardCols);
+// 		flip(theBit-boardCols);
+// 	}
 
-function flipDouble(theBit) {
-	//flip adjacent tiles
-	flip(theBit)
-	//top
-	if (grid[theBit-boardCols]+1) {
-		flip(theBit-boardCols);
-		flip(theBit-boardCols);
-	}
+// 	//bottom
+// 	if (grid[theBit+boardCols]+1) {
+// 		flip(theBit+boardCols);
+// 		flip(theBit+boardCols);
+// 	}
 
-	//bottom
-	if (grid[theBit+boardCols]+1) {
-		flip(theBit+boardCols);
-		flip(theBit+boardCols);
-	}
+// 	//left
+// 	if (grid[theBit-1]+1 && getRow(theBit) === getRow(theBit-1)) {
+// 		flip(theBit-1);
+// 		flip(theBit-1);
+// 	}
 
-	//left
-	if (grid[theBit-1]+1 && getRow(theBit) === getRow(theBit-1)) {
-		flip(theBit-1);
-		flip(theBit-1);
-	}
+// 	//right
+// 	if (grid[theBit+1]+1 && getRow(theBit) === getRow(theBit+1)) {
+// 		flip(theBit+1);
+// 		flip(theBit+1);
+// 	}
+// }
 
-	//right
-	if (grid[theBit+1]+1 && getRow(theBit) === getRow(theBit+1)) {
-		flip(theBit+1);
-		flip(theBit+1);
-	}
-}
+// function flip(theBit) {
+// 	grid[theBit] = (grid[theBit] + 1) % variety;
+// 	$('.'+theBit).css('background-color', colors[grid[theBit]%variety]);
+// }
 
-function flip(theBit) {
-	grid[theBit] = (grid[theBit] + 1) % variety;
-	$('.'+theBit).css('background-color', colors[grid[theBit]%variety]);
-}
+// function reverseFlipAdjacent(theBit) {
+// 	//flip adjacent tiles
+// 	reverseFlip(theBit);
+// 	//top
+// 	if (grid[theBit-boardCols]+1) {
+// 		reverseFlip(theBit-boardCols);
+// 	}
 
-function reverseFlipAdjacent(theBit) {
-	//flip adjacent tiles
-	reverseFlip(theBit);
-	//top
-	if (grid[theBit-boardCols]+1) {
-		reverseFlip(theBit-boardCols);
-	}
+// 	//bottom
+// 	if (grid[theBit+boardCols]+1) {
+// 		reverseFlip(theBit+boardCols);
+// 	}
 
-	//bottom
-	if (grid[theBit+boardCols]+1) {
-		reverseFlip(theBit+boardCols);
-	}
+// 	//left
+// 	if (grid[theBit-1]+1 && getRow(theBit) === getRow(theBit-1)) {
+// 		reverseFlip(theBit-1);
+// 	}
 
-	//left
-	if (grid[theBit-1]+1 && getRow(theBit) === getRow(theBit-1)) {
-		reverseFlip(theBit-1);
-	}
+// 	//right
+// 	if (grid[theBit+1]+1 && getRow(theBit) === getRow(theBit+1)) {
+// 		reverseFlip(theBit+1);
+// 	}
+// }
 
-	//right
-	if (grid[theBit+1]+1 && getRow(theBit) === getRow(theBit+1)) {
-		reverseFlip(theBit+1);
-	}
-}
+// function reverseFlipStar(theBit) {
+// 	reverseFlip(theBit);
+// 	//above
+// 	var i = 1;
+// 	while (grid[theBit-boardCols*i]+1) {
+// 		reverseFlip(theBit-boardCols*i);
+// 		i++;
+// 	}
 
-function reverseFlipStar(theBit) {
-	reverseFlip(theBit);
-	//above
-	var i = 1;
-	while (grid[theBit-boardCols*i]+1) {
-		reverseFlip(theBit-boardCols*i);
-		i++;
-	}
+// 	//below
+// 	i = 1;
+// 	while (grid[theBit+boardCols*i]+1) {
+// 		reverseFlip(theBit+boardCols*i);
+// 		i++;
+// 	}
 
-	//below
-	i = 1;
-	while (grid[theBit+boardCols*i]+1) {
-		reverseFlip(theBit+boardCols*i);
-		i++;
-	}
+// 	//left
+// 	i = 1;
+// 	while (grid[theBit-i]+1 && getRow(theBit) === getRow(theBit-i)) {
+// 		reverseFlip(theBit-i);
+// 		i++;
+// 	}
 
-	//left
-	i = 1;
-	while (grid[theBit-i]+1 && getRow(theBit) === getRow(theBit-i)) {
-		reverseFlip(theBit-i);
-		i++;
-	}
+// 	//right
+// 	i = 1;
+// 	while (grid[theBit+i]+1 && getRow(theBit) === getRow(theBit+i)) {
+// 		reverseFlip(theBit+i);
+// 		i++;
+// 	}
+// }
 
-	//right
-	i = 1;
-	while (grid[theBit+i]+1 && getRow(theBit) === getRow(theBit+i)) {
-		reverseFlip(theBit+i);
-		i++;
-	}
-}
+// function reverseFlipDouble(theBit) {
+// 	//flip adjacent tiles
+// 	reverseFlip(theBit);
+// 	//top
+// 	if (grid[theBit-boardCols]+1) {
+// 		reverseFlip(theBit-boardCols);
+// 		reverseFlip(theBit-boardCols);
+// 	}
 
-function reverseFlipDouble(theBit) {
-	//flip adjacent tiles
-	reverseFlip(theBit);
-	//top
-	if (grid[theBit-boardCols]+1) {
-		reverseFlip(theBit-boardCols);
-		reverseFlip(theBit-boardCols);
-	}
+// 	//bottom
+// 	if (grid[theBit+boardCols]+1) {
+// 		reverseFlip(theBit+boardCols);
+// 		reverseFlip(theBit+boardCols);
+// 	}
 
-	//bottom
-	if (grid[theBit+boardCols]+1) {
-		reverseFlip(theBit+boardCols);
-		reverseFlip(theBit+boardCols);
-	}
+// 	//left
+// 	if (grid[theBit-1]+1 && getRow(theBit) === getRow(theBit-1)) {
+// 		reverseFlip(theBit-1);
+// 		reverseFlip(theBit-1);
+// 	}
 
-	//left
-	if (grid[theBit-1]+1 && getRow(theBit) === getRow(theBit-1)) {
-		reverseFlip(theBit-1);
-		reverseFlip(theBit-1);
-	}
+// 	//right
+// 	if (grid[theBit+1]+1 && getRow(theBit) === getRow(theBit+1)) {
+// 		reverseFlip(theBit+1);
+// 		reverseFlip(theBit+1);
+// 	}
+// }
 
-	//right
-	if (grid[theBit+1]+1 && getRow(theBit) === getRow(theBit+1)) {
-		reverseFlip(theBit+1);
-		reverseFlip(theBit+1);
-	}
-}
+// function reverseFlip(theBit) {
+// 	grid[theBit] = (grid[theBit] - 1);
 
-function reverseFlip(theBit) {
-	grid[theBit] = (grid[theBit] - 1);
+// 	if (grid[theBit] < 0) {
+// 		grid[theBit] = variety - 1;
+// 	}
 
-	if (grid[theBit] < 0) {
-		grid[theBit] = variety - 1;
-	}
+// 	$('.'+theBit).css('background-color', colors[grid[theBit] % variety]);
+// }
 
-	$('.'+theBit).css('background-color', colors[grid[theBit] % variety]);
-}
+// function solved() {
+// 	return grid.every(bit => bit === 0);
+// }
 
-function solved() {
-	return grid.every(bit => bit === 0);
-}
+// function getRow(bit) {
+// 	for (var i = 0; i < boardRows; i++) {
+// 		if (bit < i * boardCols) {
+// 			return i-1;
+// 		}
+// 	}
 
-function getRow(bit) {
-	for (var i = 0; i < boardRows; i++) {
-		if (bit < i * boardCols) {
-			return i-1;
-		}
-	}
+// 	return i-1;
+// }
 
-	return i-1;
-}
-
-function randomize() {
-	var boardSize = boardRows * boardCols;
-	for(var i = 0; i < boardSize * 5; i++) {
-		if(mode === 0) {
-			reverseFlipAdjacent(rand(boardSize));
-		}
-		else if(mode === 1) {
-			reverseFlipStar(rand(boardSize));
-		}
-		else if (mode === 2) {
-			reverseFlipDouble(rand(boardSize));
-		}
-	}
-}
-
-function rand(max) {
-	return Math.floor(Math.random() * (max));
-}
+// function randomize() {
+// 	var boardSize = boardRows * boardCols;
+// 	for(var i = 0; i < boardSize * 5; i++) {
+// 		if(mode === 0) {
+// 			reverseFlipAdjacent(rand(boardSize));
+// 		}
+// 		else if(mode === 1) {
+// 			reverseFlipStar(rand(boardSize));
+// 		}
+// 		else if (mode === 2) {
+// 			reverseFlipDouble(rand(boardSize));
+// 		}
+// 	}
+// }
 
 function genPermalink() {
 	var link = `?${mode}+${variety}+${boardRows}+${boardCols}+`;
